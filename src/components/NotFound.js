4c39c5242ba3b30519baff5d0e83af20bbe4104e
Carly Moses <cmoses916@gmail.com>
2020-07-25 17:27:11 -0400

    Revise styles in Not Found

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 class NotFound extends React.Component {
-  paragraphStyle = {
-    textAlign: 'center'
-  };
-  
   render() {
-    return <p style={this.paragraphStyle}>Sorry about your luck. Page not found.</p>;
+    const paragraphStyle = {
+      textAlign: 'center',
+    };
+
+    return (
+      <div>
+        <p style={paragraphStyle}> Sorry about your luck. Page not found.</p>
+      </div>
+    );
   }
 }
 
