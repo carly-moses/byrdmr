1ddfdc3b6bc39411fbafe7cf370b7dbf885cf3cc
Carly Moses <cmoses916@gmail.com>
2020-07-26 16:53:07 -0400

    Desktop styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Intro from './Intro/Intro';
-import ContractForm from './ContactForm/ContactForm';
 
 class App extends React.Component {
   render() {
     return (
       <span>
         <Intro />
-        <ContractForm />
       </span>
     );
   }
