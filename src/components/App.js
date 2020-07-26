e7a645c7edbcd73a7ac0ad9897b233fbf5479b94
Carly Moses <cmoses916@gmail.com>
2020-07-26 13:47:56 -0400

    Form functionality and mobile styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Intro from './Intro/Intro';
+import ContractForm from './ContactForm/ContactForm';
 
 class App extends React.Component {
   render() {
     return (
       <span>
         <Intro />
+        <ContractForm />
       </span>
     );
   }
